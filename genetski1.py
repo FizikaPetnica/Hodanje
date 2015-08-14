ec40c1b4eccf1001eaea88d09f6128840b5d239b
Maja Pantic <pantic.maja0@gmail.com>
2015-08-14 15:56:14 +0200

    Dodati covek i genetski

---
+++ genetski1.py
@@ -0,0 +1,286 @@
+import pygame
+from pygame.locals import *
+
+import Box2D
+from Box2D.b2 import *
+
+import math
+
+import numpy as np
+
+TARGET_FPS=60
+PPM=20.0
+TIME_STEP=1.0/TARGET_FPS
+SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 600
+
+colors = {
+    staticBody  : (255,255,255,255),
+    dynamicBody : (127,127,127,255),
+}
+
+W = 15 * np.random.rand(20, 8, 4) - 7.5
+M = np.zeros((4,))
+ocena = np.zeros((20,), dtype=[('x', int),('y', int)])
+
+world=world(gravity=(0,-9.81),doSleep=True)
+
+ground=world.CreateStaticBody(position=(30,2))
+box=ground.CreatePolygonFixture(box=(30,3), density=1, friction=1)
+box.filterData.categoryBits = 1
+box.filterData.maskBits = 14
+
+#clock=pygame.time.Clock()
+
+ang1 = -pi/12
+ang2 = pi/12
+ang3 = -pi/12
+ang4 = pi/12
+
+##if (10.8-2.8*math.cos(ang1)-2.8*math.cos(ang3))<(10.8-2.8*math.cos(ang2)-2.8*math.cos(ang4)):
+##    hi = 7.2+2.8*math.cos(ang1)+2.8*math.cos(ang3)
+##else:
+##    hi = 7.2+2.8*math.cos(ang2)+2.8*math.cos(ang4)
+hi = 12.6
+
+torso=world.CreateDynamicBody(position=(5,hi), angle=0)
+box=torso.CreatePolygonFixture(box=(0.2,2), density=1)
+box.filterData.categoryBits = 2
+box.filterData.maskBits = 13
+
+l1 = 5+1.4*math.sin(ang1)
+l2 = hi-2-1.4*math.cos(ang1)
+left_leg=world.CreateDynamicBody(position=(l1,l2), angle = ang1)
+box=left_leg.CreatePolygonFixture(box=(0.2,1.4), density=1)
+box.filterData.categoryBits = 4
+box.filterData.maskBits = 7
+
+l1 = 5+1.4*math.sin(ang2)
+l2 = hi-2-1.4*math.cos(ang2)
+right_leg=world.CreateDynamicBody(position=(l1,l2), angle = ang2)
+box=right_leg.CreatePolygonFixture(box=(0.2,1.4), density=1)
+box.filterData.categoryBits = 8
+box.filterData.maskBits = 11
+
+l1 = 5+2.8*math.sin(ang1)+1.4*math.sin(ang3)
+l2 = hi-2-2.8*math.cos(ang1)-1.4*math.cos(ang3)
+left_underknee=world.CreateDynamicBody(position=(l1,l2), angle = ang3)
+box=left_underknee.CreatePolygonFixture(box=(0.2,1.4), density=1)
+box.filterData.categoryBits = 4
+box.filterData.maskBits = 7
+
+l1 = 5+2.8*math.sin(ang2)+1.4*math.sin(ang4)
+l2 = hi-2-2.8*math.cos(ang2)-1.4*math.cos(ang4)
+right_underknee=world.CreateDynamicBody(position=(l1,l2), angle = ang4)
+box=right_underknee.CreatePolygonFixture(box=(0.2,1.4), density=1)
+box.filterData.categoryBits = 8
+box.filterData.maskBits = 11
+
+        ##l1 = 5+2.8*math.sin(ang1)+2.8*math.sin(ang3)-0.2*math.cos(ang3)+0.72
+        ##l2 = hi-2-2.8*math.cos(ang1)-2.8*math.cos(ang3)
+        ##left_foot=world.CreateDynamicBody(position=(l1,l2), angle=0)
+        ##box=left_foot.CreatePolygonFixture(box=(0.72,0.2), density=1)
+        ##box.filterData.categoryBits = 4
+        ##box.filterData.maskBits = 7
+        ##
+        ##l1 = 5+2.8*math.sin(ang2)+2.8*math.sin(ang4)-0.2*math.cos(ang4)+0.72
+        ##l2 = hi-2-2.8*math.cos(ang2)-2.8*math.cos(ang4)
+        ##right_foot=world.CreateDynamicBody(position=(l1,l2), angle=0)
+        ##box=right_foot.CreatePolygonFixture(box=(0.72,0.2), density=1)
+        ##box.filterData.categoryBits = 8
+        ##box.filterData.maskBits = 11
+
+        #torso.ApplyLinearImpulse(impulse=(100,0), point=torso.worldCenter, wake = True)
+        #left_underknee.ApplyLinearImpulse(impulse=(100,0), point=left_underknee.worldCenter, wake = True)
+        #world.CreateDistanceJoint(bodyA=ground_body, bodyB=dynamic_body, anchorA=ground_body.worldCenter, anchorB=dynamic_body.worldCenter)
+
+left_hip=world.CreateRevoluteJoint(
+    bodyA=torso, 
+    bodyB=left_leg, 
+    anchor=(5,hi-2),
+    lowerAngle = -ang1-pi*0.05,
+    upperAngle = pi-ang1-pi*0.05,
+    enableLimit = True,
+    maxMotorTorque = 0.0,
+    motorSpeed = 1e3,
+    enableMotor = True,
+    )
+    
+right_hip=world.CreateRevoluteJoint(
+    bodyA=torso, 
+    bodyB=right_leg, 
+    anchor=(5,hi-2),
+    lowerAngle = -ang2-pi*0.05,
+    upperAngle = pi-ang2-pi*0.05,
+    enableLimit = True,
+    maxMotorTorque = 0.0,
+    motorSpeed = 1e3,
+    enableMotor = True,
+    )
+
+left_knee=world.CreateRevoluteJoint(
+    bodyA=left_leg, 
+    bodyB=left_underknee, 
+    anchor=(5+2.8*math.sin(ang1),hi-2-2.8*math.cos(ang1)),
+    lowerAngle = ang1-ang3-pi+0.05*pi,
+    upperAngle = ang1-ang3,
+    enableLimit = True,
+    maxMotorTorque = 0.0,
+    motorSpeed = 1e3,
+    enableMotor = True,
+    )
+
+right_knee=world.CreateRevoluteJoint(
+    bodyA=right_leg, 
+    bodyB=right_underknee, 
+    anchor=(5+2.8*math.sin(ang2),hi-2-2.8*math.cos(ang2)),
+    lowerAngle = ang2-ang4-pi+0.05*pi,
+    upperAngle = ang2-ang4,
+    enableLimit = True,
+    maxMotorTorque = 0.0,
+    motorSpeed = 1e3,
+    enableMotor = True,
+    )
+
+        ##left_ankle=world.CreateRevoluteJoint(
+        ##    bodyA=left_underknee, 
+        ##    bodyB=left_foot, 
+        ##    anchor=(5+2.8*math.sin(ang1)+2.8*math.sin(ang3),hi-2-2.8*math.cos(ang1)-2.8*math.cos(ang3)),
+        ##    lowerAngle = ang3-0.5*pi+0.05*pi,
+        ##    upperAngle = ang3+0.5*pi-1,
+        ##    enableLimit = True,
+        ##    maxMotorTorque = 0.0,
+        ##    motorSpeed = 1e3,
+        ##    enableMotor = True,
+        ##    )
+        ##
+        ##right_ankle=world.CreateRevoluteJoint(
+        ##    bodyA=right_underknee, 
+        ##    bodyB=right_foot, 
+        ##    anchor=(5+2.8*math.sin(ang2)+2.8*math.sin(ang4),hi-2-2.8*math.cos(ang2)-2.8*math.cos(ang4)),
+        ##    lowerAngle = ang4-0.5*pi+0.05*pi,
+        ##    upperAngle = ang4+0.5*pi-1,
+        ##    enableLimit = True,
+        ##    maxMotorTorque = 0.0,
+        ##    motorSpeed = 1e3,
+        ##    enableMotor = True,
+        ##    )
+        
+for i in range(50):
+    
+    for j in range(20):
+
+        screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT), 0, 32)
+
+        torso.position = (5,hi)
+        torso.angle = 0
+        left_leg.position = (5+1.4*math.sin(ang1),hi-2-1.4*math.cos(ang1))
+        left_leg.angle = ang1
+        right_leg.position = (5+1.4*math.sin(ang2),hi-2-1.4*math.cos(ang2))
+        right_leg.angle= ang2
+        left_underknee.position = (5+2.8*math.sin(ang1)+1.4*math.sin(ang3),hi-2-2.8*math.cos(ang1)-1.4*math.cos(ang3))
+        left_underknee.angle = ang3
+        right_underknee.position = (5+2.8*math.sin(ang2)+1.4*math.sin(ang4),hi-2-2.8*math.cos(ang2)-1.4*math.cos(ang4))
+        right_underknee.angle = ang4
+
+        running=True
+        cnt = 0
+        while running:
+            for event in pygame.event.get():
+                if event.type==QUIT or (event.type==KEYDOWN and event.key==K_ESCAPE):
+                    running=False
+            if cnt==600 or torso.worldCenter.y<6:
+                running=False
+            cnt += 1
+            #screen.fill((20,40,60,0))
+
+            # left_leg.ApplyTorque(500, wake=True)
+            # right_leg.ApplyTorque(-200, wake=True)
+            # torso.ApplyForce(force=(150,150), point=torso.worldCenter, wake=True)
+
+            # zglob.angle - read only
+            # zglob.speed - read only
+            # zglob.GetMotorTorque - read only
+            # zglob.motorspeed - read-write
+            # zglob.maxMotorTorque - write only
+
+            polozaj = np.array([left_hip.angle, right_hip.angle, left_knee.angle, right_knee.angle, left_hip.speed, right_hip.speed, left_knee.speed, right_knee.speed])
+            
+            M = np.dot(polozaj, W[j,:,:])
+##            for k5 in range(4):
+##                if np.absolute(M[k5])>20:
+##                    M[k5] = 20*np.sign(M[k5])
+            if M[0]>0:
+                left_hip.maxMotorTorque = M[0]
+                left_hip.motorspeed = 1e3
+            else:
+                left_hip.maxMotorTorque = -M[0]
+                left_hip.motorspeed = -1e3
+            if M[1]>0:
+                right_hip.maxMotorTorque = M[1]
+                right_hip.motorspeed = 1e3
+            else:
+                right_hip.maxMotorTorque = -M[1]
+                right_hip.motorspeed = -1e3
+            if M[2]>0:
+                left_knee.maxMotorTorque = M[2]
+                left_knee.motorspeed = 1e3
+            else:
+                left_knee.maxMotorTorque = -M[2]
+                left_knee.motorspeed = -1e3
+            if M[3]>0:
+                right_knee.maxMotorTorque = M[3]
+                right_knee.motorspeed = 1e3
+            else:
+                right_knee.maxMotorTorque = -M[3]
+                right_knee.motorspeed = -1e3         
+    
+            for body in (ground, torso, left_leg, right_leg, left_underknee, right_underknee):#, left_foot, right_foot):
+                for fixture in body.fixtures:
+    
+                    shape=fixture.shape
+             
+                    vertices=[(body.transform*v)*PPM for v in shape.vertices]
+
+                    vertices=[(v[0], SCREEN_HEIGHT-v[1]) for v in vertices]
+
+                    #pygame.draw.polygon(screen, colors[body.type], vertices)
+            
+            world.Step(TIME_STEP, 10, 10)
+
+            #kicma tri dela, ruke dva dela, vrat i glava, neuronska, genet, 
+            
+            #pygame.display.flip()
+            #clock.tick(TARGET_FPS)
+
+        ocena[j] = (torso.worldCenter.x+2*(torso.worldCenter.y-5), j)
+        pygame.quit()
+
+    cnt1 = 0
+    ocena.sort(kind = 'heapsort', order='x')
+    for k in range(5):
+        for k1 in range(8):
+            for k2 in range(4):
+                if np.random.random()>0.5:
+                    W[ocena[k][1]][k1][k2]=W[ocena[10+k][1]][k1][k2]
+                else:
+                    W[ocena[k][1]][k1][k2]=W[ocena[19-k][1]][k1][k2]
+                if np.random.random()<0.005:
+                    W[ocena[k][1]][k1][k2]=np.random.random()*15-7.5
+    for k in range(4):
+        for k0 in range(4-k):
+            for k1 in range(8):
+                for k2 in range(4):
+                    if np.random.random()>0.5:
+                        W[ocena[5+cnt1][1]][k1][k2]=W[ocena[15+k][1]][k1][k2]
+                    else:
+                        W[ocena[5+cnt1][1]][k1][k2]=W[ocena[19-k0][1]][k1][k2]
+                    if np.random.random()<0.005:
+                        W[ocena[5+cnt1][1]][k1][k2]=np.random.random()*15-7.5
+            cnt1 += 1
+    print(i)
+
+print (W[ocena[19][1]][:][:])
+
+    
+    
+
